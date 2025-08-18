b89e9b98702a40d33793e40dea8416c734d38e74
aAmareus <gonzalezamareus@gmail.com>
2025-08-17 21:56:49 -0400

    Se agregó menú lateral izquierdo (sidebar). Por terminar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,39 +2,39 @@ const wrapper = document.querySelector('.category-wrapper');
 const categories = Array.from(document.querySelectorAll('.category'));
 const rightArrow = document.querySelector(".scrollBtn.arrow.right");
 const leftArrow = document.querySelector(".scrollBtn.arrow.left");
+const mainTags = document.querySelector(".main-tags");
 
 
 let currentIndex = 0;
 
-const visibleWidth = DocumentFragment.querySelector('.main-tags').offsetWidth;
-
-const positions = categories.map((cat, i) => {
-    return i == 0 ? 0 : categories[i - 1].offsetLeft + categories[i - 1].offsetWidth + 10;
-});
-
-//Functionss
-
+function getPositions() {
+    return categories.map((cat, i) => i === 0 ? 0 : cat.offsetLeft);
+}
+ 
+//Functions
 function moveCards(index) {
-    const maxScroll = wrapper.scrollWidth - visibleWidth;
+    const visibleWidth = mainTags.offsetWidth;
+    const positions = getPositions();
+    const maxScroll = wrapper.scrollWidth -visibleWidth;
     let left = positions[index] || 0;
     if (left > maxScroll) left = maxScroll;
     wrapper.style.left = -left + "px";
-    checkVisibility(left, maxScroll)
+    checkVisibility(left, maxScroll); 
 }
 
 function checkVisibility(left, maxScroll) {
-    leftArrow.style.opacity = left <= 0 ? '0' : '1';
-    rightArrow.style.opacity = left >= maxScroll ? '0' : '1';
-}
+    left.style.opacity = left <= 0 ? '0.5' : '1';
+    rightArrow.style.opacity = left >= maxScroll ? '0.5' : '1';
+} 
 
 leftArrow.onclick = () => {
     if (currentIndex > 0) currentIndex--;
-    moveCards(currentIndex)
-}
+    moveCards(currentIndex);
+};
 
 rightArrow.onclick = () => {
-    if (currentIndex < categories.lenght - 1) currentIndex++;
-    moveCards(currentIndex)
-}
+    if (currentIndex < categories.length - 1) currentIndex++;
+    moveCards(currentIndex);
+};
 
 moveCards(currentIndex);
